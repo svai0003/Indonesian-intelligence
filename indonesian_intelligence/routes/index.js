b1876611f9eb136661a422b4a73c3b8587d0501d
Libertawang <libertawang@.github.com>
2020-08-31 00:53:14 +1000

    More routers

--- indonesian_intelligence/routes/index.js
+++ indonesian_intelligence/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Indonesian Intelligence' });
+});
+
+/* GET game page. */
+router.get('/c', function(req, res, next) {
+  res.render('panjat_pinang', { title: 'Panjat Pinang' });
 });
 
 module.exports = router;
