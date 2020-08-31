f9a84d13b07a7ccdc4cad4180da8cac3d0498355
Libertawang <libertawang@.github.com>
2020-09-01 06:44:14 +1000

    Quiz feature

---
+++ indonesian_intelligence/public/js/quiz.js
@@ -0,0 +1,81 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuesions = [];
+
+let questions = [
+  {
+    question: "Is Harum a boy or a girl?",
+    choice1: "Boy",
+    choice2: "Girl",
+    answer: 2
+  },
+  {
+    question: "Is Revanno a boy or a girl?",
+    choice1: "Boy",
+    choice2: "Girl",
+    answer: 1
+  },
+  {
+    question: "Is Tumiran a boy or a girl?",
+    choice1: "Boy",
+    choice2: "Girl",
+    answer: 2
+  },
+];
+
+//CONSTANTS
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuesions = [...questions];
+  getNewQuestion();
+};
+
+getNewQuestion = () => {
+  if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    //go to the end page
+    return questionCounter = 0;
+  }
+  questionCounter++;
+  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+  currentQuestion = availableQuesions[questionIndex];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach(choice => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+
+  availableQuesions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+  choice.addEventListener("click", e => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
+});
+
+startGame();
