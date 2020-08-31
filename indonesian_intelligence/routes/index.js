8a104bcb7c09e2c55ba08f7441efe853b4b2022e
Libertawang <libertawang@.github.com>
2020-09-01 02:32:52 +1000

    New template

--- indonesian_intelligence/routes/index.js
+++ indonesian_intelligence/routes/index.js
@@ -6,6 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Indonesian Intelligence' });
 });
 
+
 /* GET game page. */
 router.get('/panjatpinang', function(req, res, next) {
   res.render('panjat_pinang', { title: 'Panjat Pinang' });
