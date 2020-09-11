f0e3c30ab1164f5da5bc8ecaa647ac3e7d444a9c
Libertawang <libertawang@.github.com>
2020-09-11 14:44:40 +1000

    link pages with each other

--- indonesian_intelligence/routes/index.js
+++ indonesian_intelligence/routes/index.js
@@ -6,6 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Indonesian Intelligence' });
 });
 
+/* GET weather page. */
+router.get('/weather', function(req, res, next) {
+  res.render('weather', { title: 'Weather' });
+});
 
 /* GET game page. */
 router.get('/panjatpinang', function(req, res, next) {
